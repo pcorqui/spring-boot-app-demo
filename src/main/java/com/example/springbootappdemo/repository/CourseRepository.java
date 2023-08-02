95cb990c080f5c37a4967082e6fc2366cac49617
paul cortes <paul.cortes@forteinnovation.mx>
2023-08-01 20:51:57 -0600

    ADD query methods

--- src/main/java/com/example/springbootappdemo/repository/CourseRepository.java
+++ src/main/java/com/example/springbootappdemo/repository/CourseRepository.java
@@ -4,6 +4,17 @@ import com.example.springbootappdemo.entity.Course;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.stream.Stream;
+
 @Repository
 public interface CourseRepository extends CrudRepository<Course,Long> {
+    //Query methods
+    //it are methods in the repository interface with speecific naming patterns
+    Iterable<Course> findAllByCategory(String category);
+    Iterable<Course> findAllByCategoryOrderByName(String category);
+    boolean existsByName(String name);
+    long countByCategory(String category);
+    Iterable<Course> findByNameOrCategory(String name, String category);
+    Iterable<Course> findByNameStartsWith(String name);
+    Stream<Course> streamAllByCategory(String category);
 }
