4785baac0f807aa94a93b92a8f690c94eef8eab4
paul cortes <paul.cortes@forteinnovation.mx>
2023-08-01 19:19:20 -0600

    Initial commit

---
+++ src/test/java/com/example/springbootappdemo/SpringBootAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
